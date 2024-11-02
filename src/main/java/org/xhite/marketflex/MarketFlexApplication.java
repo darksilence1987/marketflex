bea27a55df52e3cd03cf9e96d965b29a2e14e2b3
Kaan Kara <kaankara1987@gmail.com>
2024-11-02 20:53:58 +0300

    Add initial Spring Boot application setup
    This commit introduces a new Spring Boot application named "Market Flex". It includes the main application class and a configuration file defining the application name.

---
+++ src/main/java/org/xhite/marketflex/MarketFlexApplication.java
@@ -0,0 +1,13 @@
+package org.xhite.marketflex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketFlexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketFlexApplication.class, args);
+    }
+
+}
