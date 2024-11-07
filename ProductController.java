5c8fab6a53a0765ba278d7bce2bcf7ee3f255d17
Kaan Kara <kaankara1987@gmail.com>
2024-11-07 22:18:47 +0300

    refactor: remove outdated image handling logic from createProduct method

--- ProductController.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
-
-@PostMapping("/products/create")
-public String createProduct(@RequestParam("image") MultipartFile image, Model model) {
-    // Process the uploaded image here
-    return "redirect:/products";
-}
